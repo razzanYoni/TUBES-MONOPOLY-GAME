488b16084a266e6c96a8f6fda99c9fa40e22ac2f
Muhammad Fadhil Amri <fadhil133113@gmail.com>
2022-11-12 00:36:41 +0700

    Menambahkan fakta properti

--- src/lokasinProperti.pl
+++ src/lokasinProperti.pl
@@ -5,6 +5,81 @@
 
 
 /* Properti */
+
 /*Deklarasi Fakta*/
+/* id_properti(Properti, Nama_properti, Deskripsi_properti) : Fakta tentang identitas dari properti*/
+id_properti(a1, jakarta, ibukota_indonesia).
+id_properti(a2, hanoi, ibukota_vietnam).
+id_properti(a3, bangkok, ibukota_thailand).
+id_properti(b1, dhaka, ibukota_bangladesh).
+id_properti(b2, islamabad, ibukota_pakistan).
+id_properti(b3, new_delhi, ibukota_india).
+id_properti(c1, beijing, ibukota_china).
+id_properti(c2, seoul, ibukota_korea_selatan).
+id_properti(c3, tokyo, ibukota_jepang).
+id_properti(d1, moskow, ibukota_rusia).
+id_properti(d2, copenhagen, ibukota_denmark).
+id_properti(d3, stockholm, ibukota_swedia).
+id_properti(e1, berlin, ibukota_jerman).
+id_properti(e2, paris, ibukota_prancis).
+id_properti(e3, london, ibukota_inggris).
+id_properti(f1, roma, ibukota_italia).
+id_properti(f2, madrid, ibukota_spanyol).
+id_properti(f3, lisbon, ibukota_portugal).
+id_properti(g1, mexico_city, ibukota_meksiko).
+id_properti(g2, ottawa, ibukota_kanada).
+id_properti(g3, brasilia, ibukota_brazil).
+id_properti(h1, athena, ibukota_yunani).
+id_properti(h2, kairo, ibukota_mesir).
+
+/* harga_properti(Properti, Harga_tanah, Harga_bangunan1, Harga_bangunan2, Harga_bangunan3, Harga_landmark): Fakta tentang harga pada properti*/
+harga_properti(a1, 50, 100, 150, 200, 250).
+harga_properti(a2, 50, 100, 150, 200, 250).
+harga_properti(a3, 60, 110, 160, 210, 260).
+harga_properti(b1, 100, 150, 200, 250, 300).
+harga_properti(b2, 100, 150, 200, 250, 300).
+harga_properti(b3, 120, 170, 220, 270, 320).
+harga_properti(c1, 150, 250, 350, 450, 550).
+harga_properti(c2, 150, 250, 350, 450, 550).
+harga_properti(c3, 180, 280, 380, 480, 580).
+harga_properti(d1, 200, 300, 400, 500, 600).
+harga_properti(d2, 200, 300, 400, 500, 600).
+harga_properti(d3, 220, 320, 420, 520, 620).
+harga_properti(e1, 220, 370, 520, 670, 820).
+harga_properti(e2, 220, 370, 520, 670, 820).
+harga_properti(e3, 240, 390, 540, 690, 840).
+harga_properti(f1, 280, 430, 580, 730, 880).
+harga_properti(f2, 280, 430, 580, 730, 880).
+harga_properti(f3, 300, 450, 600, 750, 900).
+harga_properti(g1, 300, 500, 700, 900, 1100).
+harga_properti(g2, 300, 500, 700, 900, 1100).
+harga_properti(g3, 320, 520, 720, 920, 1120).
+harga_properti(h1, 360, 560, 760, 960, 1160).
+harga_properti(h2, 400, 600, 800, 1000, 1200).
+
+/* sewa_properti(Properti, Sewa_tanah, Sewa_bangunan1, Sewa_bangunan2, Sewa_bangunan3, Sewa_landmark): Fakta tentang harga sewa pada properti*/
+sewa_properti(a1, 5, 18, 30, 90, 250).
+sewa_properti(a2, 5, 18, 30, 90, 250).
+sewa_properti(a3, 10, 30, 60, 180, 450).
+sewa_properti(b1, 10, 40, 90, 270, 550).
+sewa_properti(b2, 10, 40, 90, 270, 550).
+sewa_properti(b3, 16, 50, 100, 300, 600).
+sewa_properti(c1, 16, 50, 150, 450, 750).
+sewa_properti(c2, 16, 50, 150, 450, 750).
+sewa_properti(c3, 20, 60, 180, 500, 900).
+sewa_properti(d1, 20, 70, 200, 550, 950).
+sewa_properti(d2, 20, 70, 200, 550, 950).
+sewa_properti(d3, 30, 80, 220, 600, 1000).
+sewa_properti(e1, 30, 90, 250, 700, 1050).
+sewa_properti(e2, 30, 90, 250, 700, 1050).
+sewa_properti(e3, 40, 100, 300, 750, 1100).
+sewa_properti(f1, 40, 110, 330, 800, 1150).
+sewa_properti(f2, 40, 110, 330, 800, 1150).
+sewa_properti(f3, 50, 120, 360, 850, 1200).
+sewa_properti(g1, 50, 130, 390, 900, 1275).
+sewa_properti(g2, 50, 130, 390, 900, 1275).
+sewa_properti(g3, 60, 150, 450, 1000, 1400).
+sewa_properti(h1, 60, 175, 500, 1100, 1500).
+sewa_properti(h2, 80, 200, 600, 1400, 2000).
 
 /*Deklarasi Rules*/
