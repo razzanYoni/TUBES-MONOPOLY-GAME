d8edc4187fb83b254ea220b4307048417b49cd0b
naufal <naufalfirdaus21@gmail.com>
2022-11-12 13:39:14 +0700

    layout map

---
+++ src/map.pl
@@ -0,0 +1,25 @@
+/* Lokasi */
+/*Deklarasi Fakta*/
+
+/*Deklarasi Rules*/
+
+printmap :- nl, nl,
+    write('         ____________________________________________'), nl,
+    write('        | FP | E1 | E2 | E3 | CC | F1 | F2 | F3 | WT |'), nl,
+    write('        |____|____|____|____|____|____|____|____|____|'), nl,
+    write('        | D3 |                                  | G1 |'), nl,
+    write('        |____|               ->                 |____|'), nl,
+    write('        | D2 |                                  | G2 |'), nl,
+    write('        |____|                                  |____|'), nl,
+    write('        | D1 |                                  | G3 |'), nl,
+    write('        |____|                                  |____|'), nl,
+    write('        | TX |          Let\'s Get Poor          | TX |'), nl,
+    write('        |____|                                  |____|'), nl,
+    write('        | C3 |                                  | CC |'), nl,
+    write('        |____|                                  |____|'), nl,
+    write('        | C2 |                                  | H1 |'), nl,
+    write('        |____|                                  |____|'), nl,
+    write('        | C1 |               <-                 | H2 |'), nl,
+    write('        |____|__________________________________|____|'), nl,
+    write('        | JL | B3 | B2 | B1 | CC | A3 | A2 | A1 | GO |'), nl,
+    write('        |____|____|____|____|____|____|____|____|____|').
