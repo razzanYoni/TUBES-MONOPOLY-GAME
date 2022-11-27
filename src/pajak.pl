335c865daaacd7250a9bac7d1fbd9ca88142bf8b
naufal <naufalfirdaus21@gmail.com>
2022-11-27 07:50:20 +0700

    update tax

--- src/pajak.pl
+++ src/pajak.pl
@@ -1,2 +1,14 @@
 
-:- [player].
+:- [player, lokasinProperti].
+
+% Rules
+
+/* cek posisi player apakah di tiles tax */
+isOnTax(Player) :- lokasiPemain(Player, X), (X == tx1 ; X == tx2).
+
+indeksJenis(Jenis) :- Jenis == 
+
+
+/* Menghasilkan nilai properti dalam tiles */
+
+ProValue(Tile, V) :- hargaProperti([H|T]), Tile == H , TingkatanAset(Tile, Jenis), 
